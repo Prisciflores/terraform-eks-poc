1e9ad7d997fba56dc3e8f864e2307a07fa040a7f
Priscila Flores <59884489+Prisciflores@users.noreply.github.com>
2025-05-05 17:02:34 -0400

    chore: refactor code eks vpc (#1)
    * chore: refactor code eks vpc

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket       = "mi-terraform-state-bucket" # Reemplázalo por el bucket real en us-east-1
-    key          = "terraform/eks/dev.tfstate" # Ruta dentro del bucket
-    region       = "us-east-1"                 # Región donde vas a trabajar y desplegar
-    use_lockfile = true                        # Activa bloqueo local
-    encrypt      = true                        # Cifrado activado
+    bucket       = "endlessly-radically-logical-muskox-tf-s3-state-bucket" # Reemplázalo por el bucket real en us-east-1
+    key          = "terraform/eks/dev.tfstate"                             # Ruta dentro del bucket
+    region       = "us-east-1"                                             # Región donde vas a trabajar y desplegar
+    use_lockfile = true                                                    # Activa bloqueo local
+    encrypt      = true                                                    # Cifrado activado
   }
 }
---
+++ eks.tf
@@ -0,0 +1,34 @@
+# main.tf - Crea un clúster EKS con node groups gestionados
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws" # fuente del módulo EKS oficial
+  version = "~> 20.12"                      # versión del módulo
+
+  cluster_name    = var.cluster_name    # nombre del clúster
+  cluster_version = var.cluster_version # versión de Kubernetes (1.30)
+
+  vpc_id     = module.vpc.vpc_id          # ID de la VPC existente
+  subnet_ids = module.vpc.private_subnets # IDs de subnets para EKS
+
+  # Optional
+  cluster_endpoint_public_access       = true
+  cluster_endpoint_public_access_cidrs = ["186.67.243.48/32"] # IP pública de tu máquina local
+
+
+  eks_managed_node_groups = {
+    default = {                     # node group por defecto
+      instance_types = ["t2.micro"] # tipo de instancia EC2 (free tier elegible)
+      desired_size   = 3            # número deseado de nodos
+      min_size       = 1            # tamaño mínimo
+      max_size       = 3            # tamaño máximo
+    }
+  }
+
+  #manage_aws_auth_configmap = true # gestiona aws-auth ConfigMap
+
+  tags = {
+    Environment = var.environment # etiqueta de ambiente
+    Terraform   = "true"
+  }
+  depends_on = [module.vpc] # depende del módulo VPC
+}
